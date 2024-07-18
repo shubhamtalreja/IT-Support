128a3267d5e8aa8ea520f465659e65eeaf23986b
Shubham Talreja <96420196+shubhamtalreja@users.noreply.github.com>
2024-07-18 21:54:40 +0530

    Update Db.js

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb://localhost:27017/itsupport");
+const connection = mongoose.connect("mongodb+srv://Shubham Talreja:9560249747@cluster0.wzrnop1.mongodb.net/");
 module.exports = connection;
