1e73f5cde9fdf2ca57f3754ecc297fa2135eca9e
Shubham Talreja <96420196+shubhamtalreja@users.noreply.github.com>
2024-09-30 23:03:11 +0530

    Update Db.js
    removed hardcoded mongo url

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb+srv://IT-support:9560249747@cluster0.wzrnop1.mongodb.net/test");
+const connection = mongoose.connect(process.env.MONGO_URL);
 module.exports = connection;
