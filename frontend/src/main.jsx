a188f466b32fa1afbf07d5f9678ed732a62b501c
Shubham Talreja <shubhamtalreja2@gmail.com>
2025-10-19 23:57:26 +0530

    IT-11 Migrate from CRA to Vite.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,9 +9,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <BrowserRouter>   
-<App/>
-</BrowserRouter>
-    
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
 );
 reportWebVitals();
